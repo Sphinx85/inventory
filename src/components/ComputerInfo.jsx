f4206dcf9318c4e8a9f9d230c6ff507d289400c8
kartashov.adm <igorek6500@gmaol.com>
2023-02-06 12:40:18 +0300

    new style comp info

--- src/components/ComputerInfo.jsx
+++ src/components/ComputerInfo.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
+import style from "../CSS modules/UserInfo.module.css";
+
 
 const ComputerInfo = ({computer}) => {
     return (
-        <div>
-            <div>
+        <div className={style.UserInfo}>
+            <label className={style.label} style={{fontWeight: 700}}>Имя компьютера:  </label>
+            <div className={style.info}>
                 {computer.name}
             </div>
-            <div>
+            <label className={style.label} style={{fontWeight: 700}}>Версия сборки:  </label>
+            <div className={style.info}>
                 {computer.build}
             </div>
-            <div>
+            <label className={style.label} style={{fontWeight: 700}}>Операционная система:  </label>
+            <div className={style.info}>
+
                 {computer.os}
             </div>
-            <div>
+            <label className={style.label} style={{fontWeight: 700}}>IP адрес:  </label>
+            <div className={style.info}>
                 {computer.ip}
             </div>
+
         </div>
     );
 };
